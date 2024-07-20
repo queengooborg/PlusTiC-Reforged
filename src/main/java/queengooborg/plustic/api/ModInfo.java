103b76d240cb56fb215e080a567412644cfc5a99
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-07-20 04:07:11 -0700

    Clean up dependency info in mod info

--- src/main/java/queengooborg/plustic/api/ModInfo.java
+++ src/main/java/queengooborg/plustic/api/ModInfo.java
@@ -8,16 +8,10 @@ public class ModInfo {
 	
 	// TODO add dependencies when needed
 	public static final String DEPENDS = "required-after:mantle;"
-	+ "required-after:tconstruct@[1.12-2.7.2.15,);required-after:forge@[14.23.5.2768,);"
-	+ "after:mekanism;after:biomesoplenty;"
-	+ "after:botania;after:libvulpes;after:redstoneflux;"
-	+ "after:armorplus;after:enderio;after:projectred-core;"
-	+ "after:thermalfoundation;after:substratum;after:draconicevolution;"
-	+ "after:tesla;after:baubles;after:actuallyadditions;"
-	+ "after:psi;after:avaritia;"
-	+ "after:botanicaladdons;after:survivalist;after:modernmetals;after:basemetals;"
-	+ "after:projecte;after:gemsplus;after:appliedenergistics2;"
-	+ "after:industrialforegoing;after:twilightforest@[3.7,);after:crafttweaker;"
-	+ "after:modtweaker;after:conarm;after:micsiege;after:simplyjetpacks;after:astralsorcery;after:aoa3;";
-	
+	+ "required-after:tconstruct@[1.12-2.7.2.15,);required-after:forge@[14.23.5.2768,);after:mekanism;after:aoa3;"
+	+ "after:biomesoplenty;after:botania;after:libvulpes;after:redstoneflux;after:armorplus;after:enderio;after:conarm;"
+	+ "after:projectred-core;after:thermalfoundation;after:substratum;after:draconicevolution;after:tesla;"
+	+ "after:baubles;after:actuallyadditions;after:psi;after:avaritia;after:botanicaladdons;after:survivalist;"
+	+ "after:modernmetals;after:basemetals;after:projecte;after:gemsplus;after:appliedenergistics2;after:astralsorcery;"
+	+ "after:industrialforegoing;after:twilightforest@[3.7,);after:crafttweaker;after:modtweaker;after:simplyjetpacks;";
 }
