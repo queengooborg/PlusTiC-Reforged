f7ed4b1c0ea9704e1106e71dbab4b0dd4636d726
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-08-04 00:06:55 -0700

    Add stats to Bedrock material

--- src/main/java/queengooborg/plusticreforged/materials/BedrockMaterial.java
+++ src/main/java/queengooborg/plusticreforged/materials/BedrockMaterial.java
@@ -4,7 +4,11 @@ import queengooborg.plusticreforged.Resources;
 import queengooborg.plusticreforged.api.Description;
 import queengooborg.plusticreforged.api.Material;
 import queengooborg.plusticreforged.api.MaterialColors;
+import queengooborg.plusticreforged.api.MaterialStats;
 import slimeknights.tconstruct.library.materials.definition.MaterialId;
+import slimeknights.tconstruct.tools.stats.ExtraMaterialStats;
+import slimeknights.tconstruct.tools.stats.HandleMaterialStats;
+import slimeknights.tconstruct.tools.stats.HeadMaterialStats;
 
 import java.awt.Color;
 
@@ -21,7 +25,7 @@ public class BedrockMaterial extends Material {
 			new String[]{"rock"},
 			ORDER_HARVEST + ORDER_COMPAT,
 			new MaterialColors(new Color(60, 60, 60)),
-			null,
+			new MaterialStats(new HeadMaterialStats(999999, 10, 9999, 99), new HandleMaterialStats(999999, 10, 10, 99), ExtraMaterialStats.DEFAULT),
 			Resources.getModifier("heavy_metal"),
 			1000
 		);
