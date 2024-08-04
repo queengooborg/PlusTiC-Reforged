bcccb8b40c44bb57ffc7ed1475b3bb000c721762
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-08-03 18:25:40 -0700

    Cast molten blocks back into blocks; enable metal casting

--- src/main/java/queengooborg/plusticreforged/generator/GeneratorRecipes.java
+++ src/main/java/queengooborg/plusticreforged/generator/GeneratorRecipes.java
@@ -10,11 +10,13 @@ import queengooborg.plusticreforged.Resources;
 import queengooborg.plusticreforged.api.Material;
 import queengooborg.plusticreforged.config.ModInfo;
 import slimeknights.mantle.recipe.data.ItemNameIngredient;
+import slimeknights.mantle.recipe.data.ItemNameOutput;
 import slimeknights.tconstruct.library.data.recipe.ICommonRecipeHelper;
 import slimeknights.tconstruct.library.data.recipe.IMaterialRecipeHelper;
 import slimeknights.tconstruct.library.data.recipe.ISmelteryRecipeHelper;
 import slimeknights.tconstruct.library.data.recipe.IToolRecipeHelper;
 import slimeknights.tconstruct.library.recipe.FluidValues;
+import slimeknights.tconstruct.library.recipe.casting.ItemCastingRecipeBuilder;
 import slimeknights.tconstruct.library.recipe.melting.MeltingRecipeBuilder;
 import slimeknights.tconstruct.smeltery.TinkerSmeltery;
 
@@ -51,17 +53,13 @@ public class GeneratorRecipes extends RecipeProvider implements IConditionBuilde
 		// Generate the recipes
 		for (Material material : Resources.MATERIALS) {
 			if (material.type[0].equals("metal")) {
-//				metalTagCasting(consumer, material.moltenFluid.FLUID_OBJECT, material.id, castingFolder, false);
 				metalMelting(consumer, material.moltenFluid.getFluid(), material.id, false, meltingFolder, true);
+				metalTagCasting(consumer, material.moltenFluid.FLUID_OBJECT, material.id, castingFolder, false);
 			} else {
 				MeltingRecipeBuilder.melting(ItemNameIngredient.from(material.item), material.moltenFluid.getFluid(), FluidValues.METAL_BLOCK, 1.0f).build(consumer, modResource(meltingFolder + material.id));
-//				tagCasting(consumer, material.moltenFluid.FLUID_OBJECT, FluidValues.METAL_BLOCK, TinkerSmeltery.basin, "blocks/" + material.id, castingFolder, false);
-//				tagCasting(consumer, material.moltenFluid.FLUID_OBJECT, FluidValues.INGOT, material.id, castingFolder, false);
+				ItemCastingRecipeBuilder.basinRecipe(ItemNameOutput.fromName(material.item)).setFluidAndTime(material.moltenFluid.FLUID_OBJECT, true, FluidValues.METAL_BLOCK).build(consumer, this.modResource(castingFolder + material.id));
+				tagCasting(consumer, material.moltenFluid.FLUID_OBJECT, FluidValues.INGOT, TinkerSmeltery.blankCast, castingFolder, material.id, false);
 			}
-
-			materialMeltingCasting(consumer, material.item, material.moltenFluid.FLUID_OBJECT, materialFolder);
-
-			// XXX Generate more recipes as needed here (such as blocks, ingots, nuggets, etc.)
 		}
 	}
 }
