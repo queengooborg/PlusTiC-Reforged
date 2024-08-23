b741ab481830d5057ec22c98889014884e364d80
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-08-22 19:51:36 -0700

    Start converting Bloody Mary modifier

--- src/main/java/queengooborg/plusticreforged/modifiers/BloodyMaryModifier.java
+++ src/main/java/queengooborg/plusticreforged/modifiers/BloodyMaryModifier.java
@@ -1,30 +1,38 @@
 package queengooborg.plusticreforged.modifiers;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import queengooborg.plusticreforged.api.Description;
 import queengooborg.plusticreforged.api.Modifier;
+import slimeknights.tconstruct.library.tools.context.ToolAttackContext;
+import slimeknights.tconstruct.library.tools.nbt.IModifierToolStack;
+
+import java.util.Random;
 
 public class BloodyMaryModifier extends Modifier {
+	private Random random;
 	public BloodyMaryModifier() {
 		super("bloodymary", "Bloody Mary", new Description("Brought to you by the legendary Queen of England...", "Deals bonus damage depending on the amount of health already lost by the target. Also, a chance to drop a Coagulated Blood for each hit."), 0xFF0000);
 	}
 
-//	@Override
-//	public float damage(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damage, float newDamage, boolean isCritical) {
-//		newDamage += Math.pow(target.getMaxHealth()-target.getHealth(),0.6);
-//		return super.damage(tool, player, target, damage, newDamage, isCritical);
-//	}
-//
-//	@Override
-//	public void afterHit(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damageDealt, boolean wasCritical, boolean wasHit) {
-//		if (wasHit) {
-//			spillBlood(target.getEntityWorld(),target.posX,target.posY,target.posZ,0.23f);
-//		}
-//	}
-//
-//	protected void spillBlood(World world, double x, double y, double z, float chance) {
-//		if (!world.isRemote && random.nextFloat() < chance) {
+	@Override
+	public float getEntityDamage(IModifierToolStack tool, int level, ToolAttackContext context, float baseDamage, float damage) {
+		return (float) (damage + Math.pow(context.getLivingTarget().getMaxHealth()-context.getLivingTarget().getHealth(),0.6));
+	}
+
+	@Override
+	public int afterEntityHit(IModifierToolStack tool, int level, ToolAttackContext context, float damageDealt) {
+		Entity target = context.getTarget();
+		spillBlood(target.level,target.getX(),target.getY(),target.getZ(),0.23f);
+		return 0;
+	}
+
+	protected void spillBlood(World world, double x, double y, double z, float chance) {
+		if (random.nextFloat() >= chance) {
+			// XXX Convert me!
 //			EntityItem entity = new EntityItem(world, x, y, z, TinkerCommons.matSlimeBallBlood.copy());
 //			world.spawnEntity(entity);
-//		}
-//	}
+		}
+	}
 }
