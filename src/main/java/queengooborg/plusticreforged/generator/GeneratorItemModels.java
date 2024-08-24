7949c7551618bb7357d3279ce802eebeb48402ec
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-08-23 22:40:28 -0700

    Add missing castings and assertions

--- src/main/java/queengooborg/plusticreforged/generator/GeneratorItemModels.java
+++ src/main/java/queengooborg/plusticreforged/generator/GeneratorItemModels.java
@@ -5,6 +5,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.item.BucketItem;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.CustomLoaderBuilder;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -32,10 +33,11 @@ public class GeneratorItemModels extends ItemModelProvider {
 
 	public void bucketModel(Fluid fluid) {
 		RegistryObject<BucketItem> reg = fluid.FLUID_BUCKET;
-		ModelBuilder builder = getBuilder(reg.getId().getPath()).parent(getExistingFile(new ResourceLocation(ModInfo.MOD_ID, fluid.isHot() ? "item/bucket_hot_fluid" : "item/bucket_fluid")));
+		assert reg != null;
+		ModelBuilder<ItemModelBuilder> builder = getBuilder(reg.getId().getPath()).parent(getExistingFile(new ResourceLocation(ModInfo.MOD_ID, fluid.isHot() ? "item/bucket_hot_fluid" : "item/bucket_fluid")));
 
 		// Shamelessly copied from Materialis
-		builder.customLoader((t, u) -> new CustomLoaderBuilder(((ModelBuilder) t).getLocation(), (ModelBuilder) t, (ExistingFileHelper) u) {
+		builder.customLoader((t, u) -> new CustomLoaderBuilder(((ModelBuilder<?>) t).getLocation(), t, (ExistingFileHelper) u) {
 			public JsonObject toJson(JsonObject json) {
 				json.addProperty("loader", "forge:bucket");
 				json.addProperty("fluid", reg.get().getFluid().getRegistryName().toString());
